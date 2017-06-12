b4c45163cbc2f1bfaa4af9c6ae049ccb66910505
savar19 <3884346@gmail.com>
2017-06-12 16:57:44 +0300

    settings changed

--- techscopes/settings.py
+++ techscopes/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static/'),
+    os.path.join('static/'),
 ]
 
 MEDIA_URL = '/media/'
