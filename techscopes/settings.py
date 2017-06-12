538d8e328f3f843255abe7832949b131c507ffe3
savar19 <3884346@gmail.com>
2017-06-12 15:30:09 +0300

    settings changed

--- techscopes/settings.py
+++ techscopes/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static/"),
