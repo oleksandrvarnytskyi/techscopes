03e20593a5877e058006fb00b495bab5090a4dfd
savar19 <3884346@gmail.com>
2017-06-12 17:40:22 +0300

    static moved

--- techscopes/settings.py
+++ techscopes/settings.py
@@ -130,11 +130,11 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT, '../techscopes')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 MEDIA_URL = '/media/'
