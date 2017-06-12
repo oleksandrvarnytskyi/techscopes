ff5892d555b3e34f6cd4f458e0d2f6e240030ef3
savar19 <3884346@gmail.com>
2017-06-12 21:45:48 +0300

    wsgi.py changed

--- techscopes/settings.py
+++ techscopes/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
