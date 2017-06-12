f05a6d428ccc444577b5052f2e5afa6eb3db72b6
savar19 <3884346@gmail.com>
2017-06-12 17:49:05 +0300

    urls changed

--- techscopes/settings.py
+++ techscopes/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../static'),
 )
 
 MEDIA_URL = '/media/'
--- techscopes/urls.py
+++ techscopes/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from news.views import PostsListView
 
@@ -28,6 +27,3 @@ urlpatterns = [
     url(r'^news/', include('news.urls', namespace='news')),
     url(r'^comments/', include('comments.urls', namespace='comments')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
